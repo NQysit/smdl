c030d5c5bbdb033ff72262c302b583b9977e6fa2
Margaruga <marcos.diaz.cast@gmail.com>
2016-06-09 18:23:54 +0200

    Refactor

--- get_tournament_stickers.py
+++ get_tournament_stickers.py
@@ -6,25 +6,22 @@
 # 5/6/2016
 # NQysit
 
+import os
 import time
-import json
 import pprint
 import requests
-import urllib
-import json
-import pprint
-import wget
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 
-#tournaments IDs
-#1 - 2013 DreamHack Winter
-#3 - 2014 EMS One Katowice
-#4 - 2014 ESL One Cologne
-#5 - 2014 DreamHack Winter
-#6 - 2015 ESL One Katowice
-#7 - 2015 ESL One Cologne
-#8 - 2015 DreamHack Cluj-Napoca
-#9 - 2016 MLG Columbus
+# tournaments IDs
+# 1 - 2013 DreamHack Winter
+# 3 - 2014 EMS One Katowice
+# 4 - 2014 ESL One Cologne
+# 5 - 2014 DreamHack Winter
+# 6 - 2015 ESL One Katowice
+# 7 - 2015 ESL One Cologne
+# 8 - 2015 DreamHack Cluj-Napoca
+# 9 - 2016 MLG Columbus
+
 tournaments = [
     '',
     '2013 DreamHack Winter',
@@ -37,78 +34,68 @@ tournaments = [
     '2015 DreamHack Cluj-Napoca',
     '2016 MLG Columbus'
 ]
+
 id_tournament = 3
-
-url_json = 'http://steamcommunity.com/market/search/render/?category_730_Tournament[0]=tag_Tournament' + str(id_tournament) + '&category_730_Type[0]=tag_CSGO_Tool_Sticker&appid=730'
-
-#get total items for current tournament from json
-print('Getting total items...')
+url_json = ('http://steamcommunity.com/market/search/render/'
+            '?category_730_Tournament[0]=tag_Tournament{id_tournament}'
+            '&category_730_Type[0]=tag_CSGO_Tool_Sticker&appid=730').format(id_tournament=id_tournament)
 
 r = requests.get(url_json)
 total_count = r.json()['total_count']
 
-#get steam market urls
-print('Composing market URLs...')
-
-urls = []
 items_per_page = 10
-max_for = (int(total_count / items_per_page) + 1)
-for i in range(0, max_for):
-    u = 'http://steamcommunity.com/market/search/'
-    u = u + '?category_730_Tournament[0]=tag_Tournament' + str(id_tournament)
-    u = u + '&category_730_Type[0]=tag_CSGO_Tool_Sticker&appid=730'
-    u = u + '&start=' + str(i * items_per_page) 
-    u = u + '&count=' + str(items_per_page)
-    u = u + '&sort_column=name&sort_dir=asc'
-    urls.append(u)
+total_pages = int(total_count / items_per_page) + 1
 
-#parse urls
-print('Parsing URLs...')
+url = ('http://steamcommunity.com/market/search/'
+       '?category_730_Tournament[0]=tag_Tournament{id_tournament}'
+       '&category_730_Type[0]=tag_CSGO_Tool_Sticker&appid=730'
+       '&start={start_at}'
+       '&count={items_per_page}'
+       '&sort_column=name&sort_dir=asc')
 
-market_urls = []
-names = []
-imgs_urls = []
+if not os.path.exists('stickers'):
+    os.mkdir('stickers')
 
-for url in urls:
-    
-    #wait 1 second for antiddos
-    time.sleep(1)
-    
-    rq = requests.get(url)
-    soup = BeautifulSoup(rq.text, "html5lib")
-    
+for n_page in range(total_pages):
+
+    market_urls = []
+    names = []
+    img_urls = []
+
+    rq = requests.get(url.format(id_tournament=id_tournament, start_at=n_page * items_per_page, items_per_page=items_per_page))
+    soup = BeautifulSoup(rq.text, 'html.parser')
+
     for tag in soup.find_all('span', 'market_listing_item_name'):
         names.append(tag.text)
-            
+
     for link in soup.find_all('a', 'market_listing_row_link'):
         market_urls.append(link['href'])
-        
+
     for img in soup.find_all('img', 'market_listing_item_img'):
-        imgs_urls.append(img['src'].replace('/62fx62f','/94fx94f'))
+        img_urls.append(img['src'].replace('/62fx62f', '/94fx94f'))
 
+    stickers = {}
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
-}    
+    for market_url, name, img_url in zip(market_urls, names, img_urls):
 
-#Download images and generate json
-stickers = {}
-cont = 1
-for market_url, name, img_url in zip(market_urls, names, imgs_urls):
-        
-    if name not in stickers:
+        print('Downloading ', name.split('|')[1].strip())
 
-        #stickers[name] = {'market_hash_name' : market_url, 'icon_url' : img_url}
-        stickers[name] = {'market_url' : market_url, 'img_url' : img_url}
-            
-        print('Downloading ' + str(cont) + '/' + str(total_count) + "...")
-        wget.download(img_url, './stickers/' + name + '.png')
-        print(' ')
-        
-        #wait 5 seconds for antiddos
-        time.sleep(5)
-        
-    cont = cont + 1
+        if name not in stickers:
+
+            path = 'stickers/{name}.png'.format(name=name.split('|')[1].strip())
+
+            stickers[name] = {
+                'market_url': market_url,
+                'img_url': img_url
+            }
+
+            if not os.path.isfile(path):
+                rq = requests.get(img_url, headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
+                })
+                open(path, mode='wb').write(rq.content)
+
+            time.sleep(5)
 
 open('stickers.py', mode='wt').write('stickers = ' + pprint.pformat(stickers))
 
