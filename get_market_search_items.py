9bb3c5383d99aa416c8e0d00e518df24693bd944
braisnq <bcansias@gmail.com>
2016-06-12 11:49:00 +0200

    uploaded get_market_search_items

--- get_market_search_items.py
+++ get_market_search_items.py
@@ -29,6 +29,11 @@ else:
     start = 0
     count = 100
     total_count = 1
+            
+    market_urls = []
+    names = []
+    img_urls = []
+    stickers = {}
     
     while(start < total_count):
         #wait 1 second for next json requests
@@ -47,12 +52,41 @@ else:
         start = start + count
         
         if total_count > 0:
-            market_urls = []
-            names = []
-            img_urls = []
             
             path = os.path.join('stickers', query.replace(' ', '_'))
             if not os.path.exists(path):
                 os.makedirs(path)
+            
+            soup = BeautifulSoup(results_html, 'html.parser')
 
+            for tag in soup.find_all('span', 'market_listing_item_name'):
+                names.append(tag.text)
+        
+            for link in soup.find_all('a', 'market_listing_row_link'):
+                market_urls.append(link['href'])
+        
+            for img in soup.find_all('img', 'market_listing_item_img'):
+                img_urls.append(img['src'].replace('/62fx62f', '/94fx94f'))
+                
+            for market_url, name, img_url in zip(market_urls, names, img_urls):
+
+                print('Downloading: ', name)
+        
+                if name not in stickers:
+        
+                    stickers[name] = {
+                        'market_url': market_url,
+                        'img_url': img_url
+                    }
+        
+                    rq = requests.get(img_url, headers={
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
+                    })
+        
+                    file_name = '{name}.png'.format(name=name)
+        
+                    open(os.path.join(path, file_name), mode='wb').write(rq.content)
+        
+                    time.sleep(2)
+            
 print('done!')
